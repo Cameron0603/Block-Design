08e5431eb9e6a72bfb23e34209fb5b34cf08329e
Cameron Hollis <chollis2@my.chemeketa.edu>
2022-04-23 13:34:26 -0400

    finished project

---
+++ index.js
@@ -0,0 +1,41 @@
+function print(count) {
+    let message = "";
+    let is_red = false;
+    for(let i = 0; i < count; i++) {
+        if(is_red == true) {
+            is_red = false;
+            message += '<img src="images/red.svg" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
+        } else {
+            is_red = true;
+            message += '<img src="images/white.svg" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
+        }
+        if(count == 4) {
+            if(i == 1) {
+                message += '<br>\n';
+            }
+        } else if(count == 9) {
+            if(i == 2 || i == 5 || i == 8) {
+                message += '<br>\n';
+            }
+        }
+    }
+    console.log(message);
+    document.getElementById('board').innerHTML = message;
+}
+
+function change(number) {
+    let color = document.getElementById("image" + number).src;
+    if(color.includes("images/red.svg")) {
+        document.getElementById("image" + number).src = "images/white.svg";
+    } else if(color.includes("images/white.svg")) {
+        document.getElementById("image" + number).src = "images/red_white.svg";
+    } else if(color.includes("images/red_white.svg")) {
+        document.getElementById("image" + number).src = "images/white_red.svg";
+    } else if(color.includes("images/white_red.svg")) {
+        document.getElementById("image" + number).src = "images/red_white2.svg";
+    }  else if(color.includes("images/red_white2.svg")) {
+        document.getElementById("image" + number).src = "images/white_red2.svg";
+    }  else if(color.includes("images/white_red2.svg")) {
+        document.getElementById("image" + number).src = "images/red.svg";
+    }
+}
