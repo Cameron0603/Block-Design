efc18936063189f616eac21942ecc3c752fd9435
Cameron Hollis <chollis2@my.chemeketa.edu>
2022-04-23 16:22:40 -0400

    removed six, start on white

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ function print(count) {
     let message = "";
     let is_red = false;
     for(let i = 0; i < count; i++) {
-        message += '<img src="images/red.png" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
+        message += '<img src="images/white.png" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
         if(count == 4) {
             if(i == 1) {
                 message += '<br>\n';
             }
         } else if(count == 9) {
             if(i == 2 || i == 5 || i == 8) {
-                message += '<br>\n';
-            }
-        } else if(count == 6) {
-            if(i == 2 || i == 5) {
                 message += '<br>\n';
             }
         }
@@ -30,10 +26,10 @@ function change(number) {
     } else if(color.includes("images/red_white.png")) {
         document.getElementById("image" + number).src = "images/white_red.png";
     } else if(color.includes("images/white_red.png")) {
-        document.getElementById("image" + number).src = "images/red_white2.png";
-    }  else if(color.includes("images/red_white2.png")) {
         document.getElementById("image" + number).src = "images/white_red2.png";
     }  else if(color.includes("images/white_red2.png")) {
+        document.getElementById("image" + number).src = "images/red_white2.png";
+    }  else if(color.includes("images/red_white2.png")) {
         document.getElementById("image" + number).src = "images/red.png";
     }
 }
