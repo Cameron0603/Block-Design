2c3c2ab27c6e135f7a778ac619a6c055252851d9
Cameron Hollis <chollis2@my.chemeketa.edu>
2022-04-23 14:23:08 -0400

    added rotation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function print(count) {
     let message = "";
     let is_red = false;
     for(let i = 0; i < count; i++) {
-        message += '<img src="images/red_white.svg" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
+        message += '<img src="images/red.png" id="image' + (i + 1) + '" onclick="change(' + (i + 1) + ')" class="image" alt="">\n';
         if(count == 4) {
             if(i == 1) {
                 message += '<br>\n';
@@ -23,17 +23,25 @@ function print(count) {
 
 function change(number) {
     let color = document.getElementById("image" + number).src;
-    if(color.includes("images/red.svg")) {
-        document.getElementById("image" + number).src = "images/white.svg";
-    } else if(color.includes("images/white.svg")) {
-        document.getElementById("image" + number).src = "images/red_white.svg";
-    } else if(color.includes("images/red_white.svg")) {
-        document.getElementById("image" + number).src = "images/white_red.svg";
-    } else if(color.includes("images/white_red.svg")) {
-        document.getElementById("image" + number).src = "images/red_white2.svg";
-    }  else if(color.includes("images/red_white2.svg")) {
-        document.getElementById("image" + number).src = "images/white_red2.svg";
-    }  else if(color.includes("images/white_red2.svg")) {
-        document.getElementById("image" + number).src = "images/red.svg";
+    if(color.includes("images/red.png")) {
+        document.getElementById("image" + number).src = "images/white.png";
+    } else if(color.includes("images/white.png")) {
+        document.getElementById("image" + number).src = "images/red_white.png";
+    } else if(color.includes("images/red_white.png")) {
+        document.getElementById("image" + number).src = "images/white_red.png";
+    } else if(color.includes("images/white_red.png")) {
+        document.getElementById("image" + number).src = "images/red_white2.png";
+    }  else if(color.includes("images/red_white2.png")) {
+        document.getElementById("image" + number).src = "images/white_red2.png";
+    }  else if(color.includes("images/white_red2.png")) {
+        document.getElementById("image" + number).src = "images/red.png";
+    }
+}
+
+function transform() {
+    if(document.getElementById('board').style.transform == 'rotateY(0deg) rotate(45deg)') {
+        document.getElementById('board').style.transform = 'rotateY(0deg) rotate(0deg)';
+    } else {
+        document.getElementById('board').style.transform = 'rotateY(0deg) rotate(45deg)';
     }
 }
